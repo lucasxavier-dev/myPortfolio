b3e8d19d7bbce4c3bd6c25134f975b2cbda7bca6
Lucas Xavier <lucasxavier_@outlook.com.br>
2021-01-06 05:42:48 -0300

    new release

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,10 +3,16 @@
 
  for (let card of cards) {
      card.addEventListener("click", function() {
+        const contentID = card.getAttribute("id")
+       
         modalContainer.classList.add("active");
-     })
+       
+        modalContainer.querySelector("iframe").src =`https://www.youtube.com/embed/${contentID}`;
+    })
  }
 
     document.querySelector(".closeModal").addEventListener("click", function() {
         modalContainer.classList.remove("active");
+        modalContainer.querySelector("iframe").src ="";
+        
     });
