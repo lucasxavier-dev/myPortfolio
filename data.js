46dd0a7678a363fe21cf0cd8a1a2c61c71cdae80
Lucas Xavier <lucasxavier_@outlook.com.br>
2021-01-08 23:34:19 -0300

    New server functions

---
+++ data.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: "Gp2bUX7_WIg",
+        title: "JavaScript",
+        img: "javascript",
+        // linkGithub: "",
+        // linkLinkedin: ""
+    },
+
+    {
+        id: "meJf53tvsWw",
+        title: "NodeJS",
+        img: "node",
+        // linkGithub: "",
+        // linkLinkedin: ""
+    },
+
+
+    {
+        id: "uLHFPt9B2Os",
+        title: "React Native",
+        img: "react-native",
+        // linkGithub: "",
+        // linkLinkedin: ""
+    },
+
+    {
+        id: "E1E08i2UJGI",
+        title: "ReactJS",
+        img: "reactjs",
+        // linkGithub: "",
+        // linkLinkedin: ""
+    },
+
+]
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const contentModal = require('./data')
 
 server.use(express.static('public'));
 
@@ -19,11 +20,20 @@ server.get("/", function (req, res) {
 });
 
 server.get("/projects", function (req, res) {
-    return res.render("projects")
+    return res.render("projects", {items: contentModal})
 });
 
 server.get("/about", function (req, res) {
-    return res.render("about")
+
+    const about = {
+        avatarUrl: "https://user-images.githubusercontent.com/61753821/103354040-43004c00-4a89-11eb-88e3-cb43f69c5584.jpg",
+        name: "Lucas Xavier",
+        role: 'CEO and CTO at <a href="https://compare.com.br" target="_blank">@Compare</a>',
+        description: "This is my name. Yeah, my is comum and I am the best programmer on the world (rs). i'm not the best programmer on the world. But, i love this... programming, solving problems and create solutions to problems."
+    }
+
+
+    return res.render("about", { about });
 });
 
 server.get("/contact", function (req, res) {
